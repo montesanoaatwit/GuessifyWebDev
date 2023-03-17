35008423db7c751a5a534febed2ca427e71c1d2c
petneato <alecmontesano@gmail.com>
2023-03-16 22:15:07 -0400

    initial commit

---
+++ Lobby/Lobby.js
@@ -0,0 +1,12 @@
+const createGameBtn = document.getElementById('create-game-btn');
+const joinGameBtn = document.getElementById('join-game-btn');
+const gameCodeInput = document.getElementById('game-code');
+
+createGameBtn.addEventListener('click', () => {
+	// Code to create a new game goes here
+});
+
+joinGameBtn.addEventListener('click', () => {
+	const gameCode = gameCodeInput.value;
+	// Code to join an existing game with the entered game code goes here
+});

b48cf0d24c26fb80e91d31ffbc85957f24be8370
Sam Ellington <ellingtonj@wit.edu>
2023-03-24 13:41:11 -0400

    lobby javascript working

--- samPlay/Play.js
+++ samPlay/Play.js
@@ -1,26 +1,43 @@
-const playButton = document.getElementById('play-button');
-const pauseButton = document.getElementById('pause-button');
-const songProgress = document.getElementById('song-progress');
-
-playButton.addEventListener('click', function() {
-	// code to start playing song
-});
-
-pauseButton.addEventListener('click', function() {
-	// code to pause song
-});
-
-// code to update song progress bar every second
-setInterval(function() {
-	// get current progress of song (in seconds)
-	const currentProgress = 30; // replace with actual value
+// function processInput() {
+// 	// Get the user input from the input element
+// 	const userInput = document.getElementById("songInput").value;
 	
-	// get total length of song (in seconds)
-	const totalLength = 180; // replace with actual value
+// 	// Split the user input into four separate strings
+// 	const inputSongs = userInput.split(",");
+// 	const player1 = inputSongs[0];
+// 	const player2 = inputSongs[1];
+// 	const player3 = inputSongs[2];
+// 	const player4 = inputSongs[3];
 	
-	// calculate percentage of song that has been played
-	const percentagePlayed = (currentProgress / totalLength) * 100;
-	
-	// update width of song progress bar
-	songProgress.style.width = percentagePlayed + '%';
-}, 1000);
+// 	// Construct the URL with the four inputSongs and redirect to it
+// 	const url = `./play.html/?player1=${player1}&player2=${player2}&player3=${player3}&player4=${player4}`;
+// 	window.location.href = url;
+//   }
+
+
+// Define a global variable to store the inputSongs
+let inputSongs = [];
+
+function processInput() {
+  // Get the user input from the input element
+  const userInput = document.getElementById("songInput").value;
+  
+  // Split the user input into four separate strings
+  const newInputs = userInput.split(",");
+  
+  // Add the new inputs to the global inputSongs array
+  inputSongs = inputSongs.concat(newInputs);
+  
+  // Clear the input element for the next input
+  document.getElementById("songInput").value = "";
+  
+  // If all four inputs have been submitted, redirect to the new page
+  if (inputSongs.length === 4) {
+    // Construct the URL with the four inputs and redirect to it
+    const url = `play.html?input1=${inputSongs[0]}&input2=${inputSongs[1]}&input3=${inputSongs[2]}&input4=${inputSongs[3]}`;
+    window.location.href = url;
+    
+    // Reset the inputSongs array for the next session
+    inputSongs = [];
+  }
+}

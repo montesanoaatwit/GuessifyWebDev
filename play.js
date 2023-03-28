6df4b99072012b26e373407ebf485db5d72cdd28
petneato <alecmontesano@gmail.com>
2023-03-28 14:02:51 -0400

    Class commit

--- play.js
+++ play.js
@@ -12,14 +12,29 @@ window.onload = function() {
 };
 
 function endRound() {
-  const playerV = [('input[name=player1]:checked's).val(),  ('input[name=player2]:checked').val(),  ('input[name=player3]:checked').val(),  ('input[name=player4]:checked').val()]
+  const playerz = [document.getElementsByName('player1'), document.getElementsByName('player2'), document.getElementsByName('player3'), document.getElementsByName('player4')];
+  let playerV = [0, 0, 0, 0];
+  count = 0;
 
-	for (let i = 1; i <= 4; i++) {
-    if(playerV[i] == songsR[index][1]){
+  for (let item of playerz) {
+    for (let i = 0; i < item.length; i++) {
+      if (item[i].checked) {
+        playerV[count] = i;
+        break;
+      }
+    }
+
+    count++;
+  }
+
+  for (let i = 0 ; i < 4; i++) {
+    console.log("player" + playerV[i] + "song " + songsR[index])
+    if(playerV[i] == songsR[index][1]) {
       PS[i]++;
     }
   }
-  
-  index++;
 
+  console.log(PS);
+  index++;
+  songDisplay.textContent = songsR[index]
 }

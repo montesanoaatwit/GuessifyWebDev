02612b3e0328dd6ca0f1bc80414a6f47ef7bd30e
petneato <alecmontesano@gmail.com>
2023-03-30 14:03:06 -0400

    Efficiency commit

--- lobby.js
+++ lobby.js
@@ -8,47 +8,36 @@ function shuffleArray(array) {
 
 let songs = [];
 let songCount = 0;
-let songID = 1;
 
 function addSong() {
   const input = document.getElementById("songInput");
-  let song = input.value;
-  
-  if (songCount < 2) {
-    songID = 1;
-  } else if (songCount < 4) {
-    songID = 2;
-  } else if (songCount < 6) {
-    songID = 3;
-  } else if (songCount < 8) {
-    songID = 4;
-  } else {
-    // do nothing
+  let song = input.value.trim();
+
+  if (!song) {
+    return;
   }
 
+  let songID = Math.floor(songCount / 2) + 1;
   songs.push([song, songID]);
   console.log(song);
 
   songCount++;
 
-  input.value = ""
-  if (songs.length == 8) {
-    const songsR = shuffleArray(songs);
-    console.log(songsR);
+  input.value = "";
+  if (songs.length === 8) {
+    const shuffledSongs = shuffleArray(songs);
+    console.log(shuffledSongs);
 
-    localStorage.setItem('data', JSON.stringify(songsR))
-    console.log(songsR)
-  
+    localStorage.setItem('data', JSON.stringify(shuffledSongs));
     window.location.href = "/play.html";
   }
-  
 }
 
 function handleKeyPress(event) {
   if (event.key === 'Enter') {
+    event.preventDefault(); // Prevents submitting the form unintentionally
     addSong();
   }
 }
 
 document.getElementById("songInput").addEventListener("keypress", handleKeyPress);
-
